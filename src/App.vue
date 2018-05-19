86feeafd7283cb73ee5eb6f546cf112a8e46b394
student14 <student14@pcoip.ki.agh.edu.pl>
2018-05-19 15:03:39 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>Twoj e-mail to {{ email.toLowerCase() }}</h1>
+    
+    <h1>Witaj w systemie do zapisów na zajecia</h1>
     
     <div v-if="!isAuthenticated || email.length < 5">
       <input type="email" v-model="email">
@@ -13,24 +13,6 @@
       <button @click="logOut()">Wyloguj</button>
     </div>
 
-    
-
-
-
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
